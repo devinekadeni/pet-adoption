f24b4c929e614d00a00e14ec79b0ea688a4faa66
Devin <devinekadeni@gmail.com>
2019-04-23 00:58:30 +0700

    added petfinder-client api and show its data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React from 'react'
 import { render } from 'react-dom'
+import pf from 'petfinder-client'
+
 import Pet from './Pet'
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET,
+})
 
 /* class component way */
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      pets: [],
+    }
+  }
+
+  componentDidMount() {
+    petfinder.pet.find({ output: 'full', location: 'Seattle, WA' }).then(data => {
+      let pets
+
+      if (data.petfinder.pets && data.petfinder.pets.pet) {
+        if (Array.isArray(data.petfinder.pets.pet)) {
+          pets = data.petfinder.pets.pet
+        } else {
+          pets = [data.petfinder.pets.pet]
+        }
+      } else {
+        pets = []
+      }
+      this.setState({ pets })
+    })
+  }
+
   handleTitleClick() {
     alert('you clicked the title')
   }
@@ -12,9 +44,28 @@ class App extends React.Component {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Pet name="Luna" animal="Dog" breed="Havanese" />
-        <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-        <Pet name="Doink" animal="Cat" breed="Garfield" />
+        <div>
+          {this.state.pets.map(pet => {
+            let breed
+
+            if (Array.isArray(pet.breeds.breed)) {
+              breed = pet.breeds.breed.join(', ')
+            } else {
+              breed = pet.breeds.breed
+            }
+
+            return (
+              <Pet
+                key={pet.id}
+                name={pet.name}
+                animal={pet.animal}
+                breed={breed}
+                media={pet.media}
+                location={`${pet.contact.city}, ${pet.contact.state}`}
+              />
+            )
+          })}
+        </div>
       </div>
     )
   }
