ec6f937b04f0a9e10cba6bde991ffff07a4f3635
Devin <devinekadeni@gmail.com>
2019-04-28 20:11:28 +0700

    added react portal for modals in detail

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import pf from 'petfinder-client'
 import { navigate } from '@reach/router'
+
 import Carousel from './Carousel'
+import Modal from './Modal'
 
 const petfinder = pf({
   key: process.env.API_KEY,
@@ -11,7 +13,10 @@ const petfinder = pf({
 class Details extends Component {
   state = {
     loading: true,
+    showModal: true,
   }
+
+  toggleModal = () => this.setState({ showModal: !this.state.showModal })
 
   componentDidMount() {
     petfinder.pet
@@ -49,7 +54,7 @@ class Details extends Component {
       return <h1>Loading...</h1>
     }
 
-    const { name, animal, breed, location, description, media } = this.state
+    const { name, animal, breed, location, description, media, showModal } = this.state
 
     return (
       <div className="details">
@@ -59,7 +64,17 @@ class Details extends Component {
           <h2>
             {animal} - {breed} - {location}
           </h2>
+          <button onClick={this.toggleModal}>Adopt {name}</button>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <h1>Would you like to adopt {name}?</h1>
+              <div className="buttons">
+                <button onClick={this.toggleModal}>Yes</button>
+                <button onClick={this.toggleModal}>No</button>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     )
